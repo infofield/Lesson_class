f575c19ca6e328eeb7a10b036bcb10170bb73d44
Alexey Krymov <krymovalexey@gmail.com>
2021-05-24 14:20:21 +0300

    Добавьте файлы проекта.

---
+++ Lesson_class/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_class
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            Console.ReadKey();
+        }
+    }
+}
